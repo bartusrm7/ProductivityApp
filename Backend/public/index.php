50a539ae876444f42a3ed70eeefbf756bd6b37c8
bartusrm7 <bbogucki7@gmail.com>
2026-04-14 20:34:23 +0200

    install needed packages, create index.php, .gitignore, phpunit, tests folder, src

---
+++ Backend/public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo "404 Not Found";
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        echo "405 Method Not Allowed";
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        if (is_callable($handler)) {
+            return $handler($vars);
+        }
+        [$class, $method] = $handler;
+}
