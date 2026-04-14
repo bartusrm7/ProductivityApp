61f39d10fb00c4f011b90310e6ac6e887a1fbe10
bartusrm7 <bbogucki7@gmail.com>
2026-04-14 20:56:07 +0200

    create Database.php, impelement logic to connect with db

---
+++ Backend/src/Database/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $dbhost = $_ENV['DBHOST'];
+        $dbname = $_ENV['DBNAME'];
+        $dbuser = $_ENV['DBUSER'];
+        $dbpass = $_ENV['DBPASS'];
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$dbhost;dbname=$dbname;charset=$charset";
+        $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
+
+        try {
+            $this->pdo = new PDO($dsn, $dbuser, $dbpass, $options);
+        } catch (PDOException $e) {
+            die('Error database' . $e->getMessage());
+        }
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->pdo;
+    }
+}
