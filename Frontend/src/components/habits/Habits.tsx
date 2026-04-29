0b441437cf5aa7fb5bc989997a062b679a738664
bartusrm7 <bbogucki7@gmail.com>
2026-04-29 20:06:43 +0200

    add CreateHabit.tsx, implement logic correct

--- Frontend/src/components/habits/Habits.tsx
+++ Frontend/src/components/habits/Habits.tsx
@@ -1,9 +1,13 @@
 import Sidebar from "../navigation/Sidebar";
 import NavbarMenu from "../navigation/NavbarMenu";
 import { useEffect, useState } from "react";
+import CreateHabit from "./CreateHabit";
 
 export default function Habits() {
 	const [showMenu, setShowMenu] = useState<boolean>(false);
+
+	const handleOpenModal = () => setShowMenu(true);
+	const handleCloseModal = () => setShowMenu(false);
 
 	useEffect(() => {
 		document.title = "ProductivityApp - Habits";
@@ -18,6 +22,7 @@ export default function Habits() {
 					<div className='p-3 p-md-4'>
 						<div className='d-flex justify-content-between align-items-center'>
 							<h2 className='mb-0'>My Habits</h2>
+							<CreateHabit show={showMenu} handleOpenModal={handleOpenModal} handleCloseModal={handleCloseModal} />
 						</div>
 					</div>
 				</div>
---
+++ Frontend/src/types/habits.ts
@@ -0,0 +1,6 @@
+export interface UserHabitData {
+	id: number;
+	name: string;
+	describe: string;
+	created_at: string;
+}
