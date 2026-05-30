943eac1aff4a2f424451a8caa8d81c0e0bfb3053
bartusrm7 <bbogucki7@gmail.com>
2026-05-30 10:36:54 +0200

    implement logic into GoalsModel.php

--- Backend/src/Models/GoalsModel.php
+++ Backend/src/Models/GoalsModel.php
@@ -4,7 +4,41 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use DateTime;
+
 class GoalsModel
 {
-    public function __construct(private int $id) {}
+    public function __construct(private int $id, private string $name, private string $description, private string $status, private string $type, private int $progress,  private DateTime $createdAt, private DateTime $deadline) {}
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function getProgress()
+    {
+        return $this->progress;
+    }
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+    public function getDeadline()
+    {
+        return $this->deadline;
+    }
 }
