9538969bb6055369b41da49f74606b184b3f27ec
bartusrm7 <bbogucki7@gmail.com>
2026-05-30 10:42:04 +0200

    add method to create new goal in repo

--- Backend/src/Repositories/GoalsRepository.php
+++ Backend/src/Repositories/GoalsRepository.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Repositories;
 
 use App\Database\Database;
+use App\Models\GoalsModel;
+use DateTime;
 use PDO;
 
 class GoalsRepository
@@ -16,8 +18,21 @@ class GoalsRepository
         $this->pdo = $db->getConnection();
     }
 
-    public function createNewGoalQuery(int $userId) {
-        $stmt = $this->pdo->prepare('INSERT INTO goals () VALUES ()');
-        $stmt->execute();
+    public function createNewGoalQuery(string $name, string $type, DateTime $createdAt, int $userId)
+    {
+        $stmt = $this->pdo->prepare('INSERT INTO goals (name, type, created_at, user_id) VALUES (:name, :type, :created_at, :user_id)');
+        $stmt->execute([':name' => $name, ':type' => $type, ':created_at' => $createdAt->format('Y-m-d H:i:s'), ':user_id' => $userId]);
+        $id = (int) $this->pdo->lastInsertId();
+
+        return new GoalsModel(
+            $id,
+            $name,
+            '',
+            '',
+            $type,
+            0,
+            $createdAt,
+            new DateTime
+        );
     }
 }
